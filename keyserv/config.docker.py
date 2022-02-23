ab6a5deb5c2f44b79ca0cbb879a2150da225a5a9
Victor Anjos <anjosvic@gmail.com>
2022-02-23 16:08:30 -0500

    making sure we can access the correct Kunin AI API for registration

--- keyserv/config.docker.py
+++ keyserv/config.docker.py
@@ -1,9 +1,8 @@
 # adjust the Config class below, then rename this file to config.py
 import os
-from .config import DefaultConfig as DF, ProductionConfig as PROD, DevelopmentConfig as DEV
 
 
-class DefaultConfig(DF):
+class DefaultConfig(object):
     # a decent way to generate a secret key is by running: python -c "import os; print(repr(os.urandom(24)))"
     # then pasting the output here.
     SECRET_KEY = os.environ['SECRET_KEY']
@@ -13,12 +12,16 @@ class DefaultConfig(DF):
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = "sqlite:///data/sqlite.db"
+    KUNIN_API = 'https://dev.kuninai.com'
 
 
-class ProductionConfig(PROD):
+class ProductionConfig(object):
     SQLALCHEMY_DATABASE_URI = "sqlite:////data/sqlite.db"
+    KUNIN_API = 'https://app.kuninai.com'
 
-class DevelopmentConfig(DEV):
+
+class DevelopmentConfig(object):
     DEBUG = True
     TESTING = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    KUNIN_API = 'https://dev.kuninai.com'
